80945602edd26ec1e5f284730a719c37cdc09bb1
Christopher Liu <chrisliu@cs.ucla.edu>
2024-02-14 16:04:48 -0800

    [build] add build support for out-of-tree LLVM pass

--- src/llvm/trace/InstrumentTracePass.cpp
+++ src/llvm/trace/InstrumentTracePass.cpp
@@ -1,31 +1,48 @@
+#include <llvm/ADT/ArrayRef.h>
 #include <llvm/ADT/StringRef.h>
 #include <llvm/IR/Function.h>
-#include <llvm/Pass.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
+#include <llvm/Support/Compiler.h>
 #include <llvm/Support/Debug.h>
 
 #define DEBUG_TYPE "dg-instrument-trace-pass"
 
 namespace dragongem {
 
-class InstrumentTracePass : public llvm::FunctionPass {
+class InstrumentTracePass : public llvm::PassInfoMixin<InstrumentTracePass> {
 public:
-  static char ID;
-
-  bool doInitialization(llvm::Module &M) override;
-  bool runOnFunction(llvm::Function &F) override;
+  llvm::PreservedAnalyses run(llvm::Function &f,
+                              llvm::FunctionAnalysisManager &AM);
 };
 
-bool InstrumentTracePass::doInitialization(llvm::Module &M) { return true; }
-
-bool InstrumentTracePass::runOnFunction(llvm::Function &F) {
+llvm::PreservedAnalyses
+InstrumentTracePass::run(llvm::Function &F, llvm::FunctionAnalysisManager &AM) {
   LLVM_DEBUG(llvm::dbgs() << F.getName() << '\n');
-  return true;
+  return llvm::PreservedAnalyses::all();
 }
 
-char InstrumentTracePass::ID = 0;
-static llvm::RegisterPass<InstrumentTracePass>
-    X("dg-instrument-trace-pass", "Instrumented trace pass", false, false);
-
 } // namespace dragongem
+
+llvm::PassPluginLibraryInfo getInstrumentTracePassInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "DGInstrumentTracePass", "v0.1",
+          [](llvm::PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
+                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+                  if (Name == "dg-instrument-trace-pass") {
+                    FPM.addPass(dragongem::InstrumentTracePass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getInstrumentTracePassInfo();
+}
 
 #undef DEBUG_TYPE
